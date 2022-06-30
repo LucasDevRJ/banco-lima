4248d5b19ed70b5b59d44413a8d7af0e8a001871
Lucas <lucaspereiradelima2020@gmail.com>
2022-06-30 18:40:57 -0300

    Criação da Classe Abstrata Cliente

---
+++ src/com/github/lucasdevrj/bancolima/cliente/Cliente.java
@@ -0,0 +1,8 @@
+package com.github.lucasdevrj.bancolima.cliente;
+
+public abstract class Cliente {
+
+	private InformacoesPessoais informacoesPessoais;
+	private Endereco endereco;
+	private float saldo;
+}
