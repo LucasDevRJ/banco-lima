82d6dcf3d6ff99b74db315f3693baf26e72c4212
Lucas <lucaspereiradelima2020@gmail.com>
2022-06-30 18:59:01 -0300

    Adição dos métodos Getters e construtor na classe Cliente

--- src/com/github/lucasdevrj/bancolima/cliente/Cliente.java
+++ src/com/github/lucasdevrj/bancolima/cliente/Cliente.java
@@ -1,5 +1,6 @@
 package com.github.lucasdevrj.bancolima.cliente;
 
+import com.github.lucasdevrj.bancolima.endereco.Endereco;
 import com.github.lucasdevrj.bancolima.informacoespessoais.InformacoesPessoais;
 
 public abstract class Cliente {
@@ -7,4 +8,22 @@ public abstract class Cliente {
 	private InformacoesPessoais informacoesPessoais;
 	private Endereco endereco;
 	private float saldo;
+	
+	public Cliente(InformacoesPessoais informacoesPessoais, Endereco endereco, float saldo) {
+		this.informacoesPessoais = informacoesPessoais;
+		this.endereco = endereco;
+		this.saldo = saldo;
+	}
+	
+	public InformacoesPessoais getInformacoesPessoais() {
+		return informacoesPessoais;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public float getSaldo() {
+		return saldo;
+	}
 }
