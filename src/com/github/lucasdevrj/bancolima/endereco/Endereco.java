b6f2b095372ed35a64b20a8e04e33fae0b6eec02
Lucas <lucaspereiradelima2020@gmail.com>
2022-06-30 18:55:24 -0300

    Criação da Classe Endereco, possuindo os métodos Getters e o construtor

---
+++ src/com/github/lucasdevrj/bancolima/endereco/Endereco.java
@@ -0,0 +1,45 @@
+package com.github.lucasdevrj.bancolima.endereco;
+
+public class Endereco {
+
+	private String cep;
+	private String cidade;
+	private String estado;
+	private String bairro;
+	private String rua;
+	private int numero;
+	private String referencia;
+	private String complemento;
+	
+	public String getCep() {
+		return cep;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public String getRua() {
+		return rua;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public String getReferencia() {
+		return referencia;
+	}
+	
+	public String getComplemento() {
+		return complemento;
+	}
+}
