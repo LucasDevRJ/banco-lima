d10fab28d937aecf0e74a2da6f178f26bfa6fd36
Lucas <lucaspereiradelima2020@gmail.com>
2022-06-30 19:39:28 -0300

    Criação da Classe Fisico que extende o Cliente

---
+++ src/com/github/lucasdevrj/bancolima/cliente/Fisico.java
@@ -0,0 +1,12 @@
+package com.github.lucasdevrj.bancolima.cliente;
+
+import com.github.lucasdevrj.bancolima.endereco.Endereco;
+import com.github.lucasdevrj.bancolima.informacoespessoais.InformacoesPessoais;
+
+public class Fisico extends Cliente {
+
+	public Fisico(InformacoesPessoais informacoesPessoais, Endereco endereco) {
+		super(informacoesPessoais, endereco);
+	}
+
+}
--- src/com/github/lucasdevrj/bancolima/principal/Principal.java
+++ src/com/github/lucasdevrj/bancolima/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.github.lucasdevrj.bancolima.principal;
 
 import com.github.lucasdevrj.bancolima.cliente.Cliente;
+import com.github.lucasdevrj.bancolima.cliente.Fisico;
 import com.github.lucasdevrj.bancolima.endereco.Endereco;
 import com.github.lucasdevrj.bancolima.informacoespessoais.InformacoesPessoais;
 
@@ -8,9 +9,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		InformacoesPessoais ip1 = new InformacoesPessoais("Lucas", "Pereira", "15/04/1999", "111.222.111-11", "22.111.444-1", "brasileiro");
-		Endereco ed1 = new Endereco();
+		Endereco ed1 = new Endereco("21123-555", "Rio de Janeiro", "Rio de Janeiro", "Tijuca", "Gadernal", 203, "Próximo ao posto Ipiranga", "Casa número 204");
 		
-		Cliente cl1 = new Cliente();
+		Cliente cf = new Fisico(ip1, ed1);
 	}
 
 }
