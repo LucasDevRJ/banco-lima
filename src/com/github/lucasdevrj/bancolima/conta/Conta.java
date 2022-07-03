6862a52533d115eecb1f2293c305149307c1d5b3
Lucas <lucaspereiradelima2020@gmail.com>
2022-07-03 13:54:20 -0300

    Adição de atributo na Classe Conta

--- src/com/github/lucasdevrj/bancolima/conta/Conta.java
+++ src/com/github/lucasdevrj/bancolima/conta/Conta.java
@@ -8,6 +8,7 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 import com.github.lucasdevrj.bancolima.cliente.Cliente;
+import com.github.lucasdevrj.bancolima.excecao.ContaInativa;
 import com.github.lucasdevrj.bancolima.excecao.ContaInvalida;
 import com.github.lucasdevrj.bancolima.leituraarquivo.LeituraArquivo;
 
@@ -17,6 +18,7 @@ public abstract class Conta {
 	protected float saldo;
 	private String agencia;
 	private String numero;
+	private boolean estaAtiva;
 	
 	public Conta(Cliente titular, String agencia, String numero, float saldo) throws Exception {
 		if (agencia.length() == 2 && numero.length() == 5 && saldo >= 0) {
@@ -24,29 +26,34 @@ public abstract class Conta {
 			this.agencia = agencia;
 			this.numero = numero;
 			this.saldo = saldo;
+			this.estaAtiva = true;
 		} else {
 			throw new ContaInvalida("Agência, número ou saldo informados são inválidos!\nAgência tem que ter apenas dois digitos, número apenas cinco digitos e saldo tem que ser maior ou igual a zero.");
 		}
 	}
 
-	public void deposita(float valor) throws IOException {
-		if (valor > 0) {
-			this.saldo += valor;
-			OutputStream fos = new FileOutputStream("arquivo.txt");
-			Writer wt = new OutputStreamWriter(fos);
-			BufferedWriter bw = new BufferedWriter(wt);
-			
-			bw.write("Deposito concluido com sucesso!");
-			bw.newLine();
-			bw.write("Valor do deposito: R$ " + valor);
-			bw.newLine();
-			bw.newLine();
-			
-			bw.close();
-			
-			LeituraArquivo.leArquivo();
+	public void deposita(float valor) throws IOException, Exception {
+		if (this.estaAtiva == true) {
+			if (valor > 0) {
+				this.saldo += valor;
+				OutputStream fos = new FileOutputStream("arquivo.txt");
+				Writer wt = new OutputStreamWriter(fos);
+				BufferedWriter bw = new BufferedWriter(wt);
+				
+				bw.write("Deposito concluido com sucesso!");
+				bw.newLine();
+				bw.write("Valor do deposito: R$ " + valor);
+				bw.newLine();
+				bw.newLine();
+				
+				bw.close();
+				
+				LeituraArquivo.leArquivo();
+			} else {
+				throw new IllegalArgumentException("Valor inválido para depósito!");
+			}
 		} else {
-			throw new IllegalArgumentException("Valor inválido para depósito!");
+			throw new ContaInativa("A conta está inativa!\nPreencha as informações corretamente para ativar a conta.");
 		}
 		
 	}
@@ -65,5 +72,9 @@ public abstract class Conta {
 	
 	public String getNumero() {
 		return numero;
+	}
+
+	public boolean isEstaAtiva() {
+		return estaAtiva;
 	}
 }
---
+++ src/com/github/lucasdevrj/bancolima/excecao/ContaInativa.java
@@ -0,0 +1,8 @@
+package com.github.lucasdevrj.bancolima.excecao;
+
+public class ContaInativa extends Exception {
+
+	public ContaInativa(String mensagem) {
+		super(mensagem);
+	}
+}
