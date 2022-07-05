b88f817a65e154b928bf9882c05f6b114ff3c1a4
Lucas <lucaspereiradelima2020@gmail.com>
2022-07-05 10:35:40 -0300

    Adição de mais atributos na classe ClienteJuridico e criação da classe

---
+++ src/com/github/lucasdevrj/bancolima/cliente/ClienteJuridico.java
@@ -0,0 +1,31 @@
+package com.github.lucasdevrj.bancolima.cliente;
+
+import com.github.lucasdevrj.bancolima.endereco.Endereco;
+import com.github.lucasdevrj.bancolima.informacoespessoais.InformacoesPessoais;
+
+public class ClienteJuridico extends Cliente {
+	
+	private String cnpj;
+	private String empresa;
+	private String ramo;
+
+	public ClienteJuridico(InformacoesPessoais informacoesPessoais, Endereco endereco, String cnpj, String empresa, String ramo) {
+		super(informacoesPessoais, endereco);
+		this.cnpj = cnpj;
+		this.empresa = empresa;
+		this.ramo = ramo;
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+	
+	public String getRamo() {
+		return ramo;
+	}
+
+}
