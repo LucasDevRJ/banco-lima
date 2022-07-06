c0bf7dc0c03b082ddb819e4720145ec0614c8da4
Lucas <lucaspereiradelima2020@gmail.com>
2022-07-06 19:36:23 -0300

    Alteração no método exibeContas da classe BancoLima

--- src/com/github/lucasdevrj/bancolima/banco/BancoLima.java
+++ src/com/github/lucasdevrj/bancolima/banco/BancoLima.java
@@ -1,11 +1,17 @@
 package com.github.lucasdevrj.bancolima.banco;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 import com.github.lucasdevrj.bancolima.conta.Conta;
 import com.github.lucasdevrj.bancolima.endereco.Endereco;
 import com.github.lucasdevrj.bancolima.excecao.ContasIguais;
+import com.github.lucasdevrj.bancolima.leituraarquivo.LeituraArquivo;
 
 public class BancoLima {
 
@@ -13,19 +19,29 @@ public class BancoLima {
 	private Endereco endereco;
 	private ArrayList<Conta> contas = new ArrayList<Conta>();
 	
-	public void exibeContas() throws IOException {
-		
-		for (int i = 0; i < contas.size(); i++) {
-			System.out.println(contas.toString());
-			System.out.println();
-		}
-	}
-	
 	public void adicionaConta(Conta conta) throws ContasIguais {
 		if (!this.contas.contains(conta)) {
 			this.contas.add(conta);
 		} else {
 			throw new ContasIguais("Existem contas iguais!\nNão pode adicionar mais de uma conta com o mesmo número de agência e número da conta.");
+		}
+	}
+	
+	public void exibeContas() throws IOException {
+		
+		if (this.getContas().size() > -1) {
+			OutputStream fos = new FileOutputStream("arquivo.txt");
+			Writer wt = new OutputStreamWriter(fos);
+			BufferedWriter bw = new BufferedWriter(wt);
+			
+			for (int i = 0; i < this.getContas().size(); i++) {
+				bw.write(this.contas.toString());
+				bw.newLine();
+			}
+			
+			bw.close();
+			
+			LeituraArquivo.leArquivo();
 		}
 	}
 	
